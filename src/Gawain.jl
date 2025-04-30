ee8e5a469913155ff43bfbfad7ec7b613aeb9d95
Dominique Orban <dominique.orban@gerad.ca>
2025-04-30 11:00:11 -0400

    TRB: implement callback

--- src/Gawain.jl
+++ src/Gawain.jl
@@ -11,6 +11,23 @@ using GALAHAD
 using NLPModels
 using SolverCore
 
+const Callback_docstring = "
+The callback is called at each iteration.
+The expected signature of the callback is `callback(model, solver, stats)`, and its output is ignored.
+Changing any of the input arguments will affect the subsequent iterations.
+  In particular, setting `stats.status` to anything else than `:unknown` will stop the algorithm, and setting it `:user` will signal that user-requested termination was requested.
+All relevant information should be available in `model`, `solver` and `stats`.
+Notably, you can access, and modify, the following:
+- `solver.x`: current iterate;
+- `solver.gx`: current gradient;
+- `stats`: structure holding the output of the algorithm (`GenericExecutionStats`), which contains, among other things:
+  - `stats.dual_feas`: norm of the residual, for instance, the norm of the gradient for unconstrained problems;
+  - `stats.iter`: current iteration counter;
+  - `stats.objective`: current objective function value;
+  - `stats.status`: current status of the algorithm. Should be `:unknown` unless the algorithm attained a stopping criterion. Changing this to anything will stop the algorithm, but you should use `:user` to properly indicate the intention.
+  - `stats.elapsed_time`: elapsed time in seconds.
+"
+
 include("trb.jl")
 
 end
--- test/test-cutest.jl
+++ test/test-cutest.jl
@@ -5,15 +5,30 @@ model = CUTEstModel("3PK")
 # straightfoward test
 stats = trb(model)
 @test stats.status == :first_order
+@test stats.iter == 5
 
 # test with solver object
 solver = TRBSolver(model)
 stats = GenericExecutionStats(model)
 solve!(solver, stats)
 @test stats.status == :first_order
+@test stats.iter == 5
 
 # test resolve
 reset!(solver)
 reset!(stats)
 solve!(solver, stats)
 @test stats.status == :first_order
+@test stats.iter == 5
+
+# test callback
+reset!(solver)
+reset!(stats)
+solve!(
+  solver,
+  stats,
+  callback = (model, solver, stats) ->
+    set_status!(stats, stats.iter == 3 ? :user : :unknown),
+)
+@test stats.status == :user
+@test stats.iter == 3
